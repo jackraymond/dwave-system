0a8a1b380ae3b6b20697684dd27e9a8b5fdd59fa
Jack Raymond <10591246+jackraymond@users.noreply.github.com>
2021-12-28 10:44:31 -0800

    Added scipy to requirements, allowed sampler_params as argument to fast_effective_temperature

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ install_requires = ['dimod>=0.10.0,<0.11.0',
                     'minorminer>=0.2.4,<0.3.0',
                     'numpy>=1.14.0,<2.0.0',
                     'dwave-tabu>=0.4.2',
+                    'scipy>=1.5.2,<2.0.0',
                     ]
 
 # NOTE: dwave-drivers can also be installed with `dwave install drivers`,
